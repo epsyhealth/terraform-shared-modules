4a71e52a9e34b9814e55a8c561d6831a53813fba
Michal Przytulski <michal@przytulski.pl>
2022-08-15 16:22:05 +0200

    allow dynamic policy

--- modules/kms_service_key/main.tf
+++ modules/kms_service_key/main.tf
@@ -1,6 +1,59 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  default_statements = [
+    {
+      sid       = "Enable IAM User Permissions"
+      effect    = "Allow"
+      actions   = ["kms:*"]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+        }
+      ]
+    }
+  ]
+}
+
+
+data aws_iam_policy_document "policy" {
+  dynamic "statement" {
+    for_each = concat(local.default_statements, var.key_policy_statements)
+    content {
+      sid       = statement.value["sid"]
+      effect    = statement.value["effect"]
+      actions   = statement.value["actions"]
+      resources = statement.value["resources"]
+
+      dynamic "principals" {
+        for_each = lookup(statement.value, "principals", [])
+        content {
+          type        = principals.value["type"]
+          identifiers = principals.value["identifiers"]
+        }
+      }
+
+      dynamic "condition" {
+        for_each = lookup(statement.value, "conditions", [])
+        content {
+          test     = condition.value["test"]
+          variable = condition.value["variable"]
+          values   = condition.value["values"]
+        }
+      }
+    }
+  }
+}
+
 resource "aws_kms_key" "service_key" {
   deletion_window_in_days = 14
   multi_region            = true
+  policy                  = data.aws_iam_policy_document.policy.json
 }
 
 resource "aws_kms_alias" "service_key_alias" {
--- modules/kms_service_key/variables.tf
+++ modules/kms_service_key/variables.tf
@@ -6,3 +6,7 @@ variable "stage" {
   type    = string
   default = "dev"
 }
+
+variable "key_policy_statements" {
+  type = list(map(any))
+}
